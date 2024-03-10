540718e6d29293b3faffb6cd5bf128075d796dd6
JohannDav <quirino.gonzalez.johann.david@gmail.com>
2024-03-09 19:41:04 -0600

    Agregar métodos

--- POO/src/Lógica/Alumno.java
+++ POO/src/Lógica/Alumno.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Lógica;
-
-/**
- *
- * @author LENOVO
- */
 public class Alumno {
     
+    int id;
+    String nombre;
+    String Apellido;
+    //modificador, tipo de dato que va a retornar, nombre del método, recibir un parametro?
+    public void mostrarNombre(){
+        
+        System.out.println("Hola soy un alumno y sé decir mi nombre: ");
+        
+    }
+    
+    public void saberAprobado(double calificacion){
+        
+        if(calificacion <= 6){
+            System.out.println("Aprobé la materia");
+        }
+        else{
+            System.out.println("Uyy, no aprobé");
+        }
+        
+    }
 }
