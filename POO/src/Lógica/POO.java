59844f1e00630ff4aba61df8f109f42550095bc0
JohannDav <quirino.gonzalez.johann.david@gmail.com>
2024-03-09 20:09:43 -0600

    2 Agregar constructor

--- POO/src/Lógica/POO.java
+++ POO/src/Lógica/POO.java
@@ -2,6 +2,9 @@ package Lógica;
 public class POO {
     public static void main(String[] args) {
         
+        Alumno alu1 = new Alumno ();        
+        Alumno alu2 = new Alumno (5, "Luicina", "de Paula");
+        
         
         
     }
