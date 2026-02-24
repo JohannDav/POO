18d89458342ac50c6acd707994848f4662577e3a
JohannDav <quirino.gonzalez.johann.david@gmail.com>
2026-02-23 19:37:39 -0600

    3 Hola mundo en Java

---
+++ 1P/3-HolaMundo/src/pkg3/holamundo/HolaMundo.java
@@ -0,0 +1,8 @@
+//Quirino Gonzalez Johann David
+package pkg3.holamundo;
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
