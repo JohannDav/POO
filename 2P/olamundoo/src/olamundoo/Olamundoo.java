e6c61e9fafb609afefa2d5058990dfb061698d69
JohannDav <quirino.gonzalez.johann.david@gmail.com>
2026-04-21 14:55:13 -0600

    Todo acabado hasta el 20-04-2026

---
+++ 2P/olamundoo/src/olamundoo/Olamundoo.java
@@ -0,0 +1,10 @@
+// Autor: Quirino Gonzalez Johann David
+package olamundoo;
+import java.util.Scanner;
+public class Olamundoo {
+    public static void main(String[] args) {
+        System.out.println("\nQuirino Gonzalez Johann David");
+        Scanner scan = new Scanner(System.in);
+        
+    } //Fin del void main
+}
